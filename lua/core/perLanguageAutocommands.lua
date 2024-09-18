fcc5f885d9621860b1c96a06bfa17ce5c8dd3f97
NJCsih <NJCish@gmail.com>
2024-09-18 14:38:39 -0600

    Added filetypeAcmd for verilog

--- lua/core/perLanguageAutocommands.lua
+++ lua/core/perLanguageAutocommands.lua
@@ -50,6 +50,18 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
 	end,
 })
 
+-- For verilog files
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+	pattern = { "*.v" },
+	callback = function()
+		vim.opt_local.tabstop = 4
+		vim.opt_local.softtabstop = 4
+		vim.opt_local.shiftwidth = 4
+		vim.opt_local.expandtab = false
+		vim.opt_local.colorcolumn = "100"
+	end,
+})
+
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, { pattern = { "*.txt" },
 	callback = function()
 		vim.opt_local.spell = true
