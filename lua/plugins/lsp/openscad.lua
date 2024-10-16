8121cbabdb0a1fbc576980ff1ba183119fdc2911
NJCsih <NJCish@gmail.com>
2024-10-16 09:54:59 -0600

    Openscad lsp progress?

--- lua/plugins/lsp/openscad.lua
+++ lua/plugins/lsp/openscad.lua
@@ -1,8 +1,9 @@
--- Minimal zls config for zig language
+-- openscad lsp config
 local lspconfig = require('lspconfig')
 
 lspconfig.openscad_ls.setup({
-    cmd = { "openscad-language-server" },
-    filetypes = { "openscad" },
-    single_file_support = true,
+  cmd = { "openscad-lsp" },
+  filetypes = { "openscad", "scad" },
+  root_dir = lspconfig.util.root_pattern('.git');
+  single_file_support = true,
 })
