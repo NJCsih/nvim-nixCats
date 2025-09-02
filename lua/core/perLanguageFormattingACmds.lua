436fa03a0f08773db8a1a1c772675bfcecccf17d
NJCsih <29319746+NJCsih@users.noreply.github.com>
2025-09-02 11:29:50 -0600

    add keybinds for qwerty hjl flash/hop, tweak verilog settings

--- lua/core/perLanguageFormattingACmds.lua
+++ lua/core/perLanguageFormattingACmds.lua
@@ -87,8 +87,8 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
     vim.opt_local.tabstop = 4
     vim.opt_local.softtabstop = 4
     vim.opt_local.shiftwidth = 4
-    vim.opt_local.expandtab = false
-    vim.opt_local.colorcolumn = "80"
+    vim.opt_local.expandtab = true
+    vim.opt_local.colorcolumn = "80,100"
     vim.opt_local.textwidth = 80
   end,
 })
