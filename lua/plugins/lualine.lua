f0d0b41cb008ffd1e83eb2c16335e259aec970c5
NJCsih <29319746+NJCsih@users.noreply.github.com>
2025-09-09 00:23:14 -0600

    tweak: clean up lualine a bit

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -22,16 +22,16 @@ require('lualine').setup {
   sections = {
     lualine_a = {'mode'},
     lualine_b = {'filename'},
-    lualine_c = {'branch', 'diff', 'diagnostics'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_c = {'diff', 'diagnostics'},
+    lualine_x = {},
     lualine_y = {'progress'},
     lualine_z = {'location'}
   },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
+    lualine_c = {},
+    lualine_x = {},
     lualine_y = {},
     lualine_z = {}
   },
